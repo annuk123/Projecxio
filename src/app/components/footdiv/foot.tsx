b5235dc16d401bb72dccc58798ba081d8ca270b2
Annu Kumari <anuk35168@gmail.com>
2025-04-21 02:46:48 +0530

    bug fix: fix the bug of footdiv

--- src/app/components/footdiv/foot.tsx
+++ src/app/components/footdiv/foot.tsx
@@ -1,35 +1,3 @@
-// "use client";
-// import { ReactNode } from "react";
-
-// interface WaveDividerProps {
-//   flip?: boolean;
-
-//   fillColor?: string; // Accepts a HEX, RGB, or Tailwind-compatible color
-// }
-
-// const FootDivider = ({ flip = true,   fillColor = "#000000", }: WaveDividerProps) => (
-//   <div className="relative overflow-hidden leading-none">
-//     <svg
-//       viewBox="0 0 1440 100"
-//       className={`w-full h-[120px] ${flip ? "" : "rotate-180"}`}
-//       preserveAspectRatio="none"
-//     >
-//       <path
-//        fill={fillColor}
-//         d="M0 50 Q 180 0 360 50 T 720 50 T 1080 50 T 1440 50 L1440 100 L0 100 Z"
-//         className="fill-current text-violet-100 dark:text-zinc-900"
-//       />
-//     </svg>
-
-
-    
-    
-//   </div>
-// );
-
-// export default FootDivider;
-
-
 "use client";
 
 interface FootDividerProps {
