6931a453d1e26d6d64ff0f5e0ba509d1ea57938a
Annu Kumari <anuk35168@gmail.com>
2025-06-14 21:49:16 +0530

    Redesign the layout of the application

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,7 +15,7 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-  title: "IndieSpark | Annu Kumari",
+  title: "Projexio | Annu Kumari",
 
   description: "Portfolio of Annu Kumari",
 };
